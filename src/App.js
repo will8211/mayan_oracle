c11d53f5ce9a8a09eaf717ffa95fd263242e093d
will8211 <wmannard@gmail.com>
2021-07-17 20:07:16 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactCardFlip from 'react-card-flip';
+import back from './back.png';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+      this.state = { isFlipped: false };
+      this.handleClick = this.handleClick.bind(this);
+      const random = Math.floor(Math.random() * 4) + 1;
+      this.state = { card: process.env.PUBLIC_URL + '/cards/' + random + '.png' };
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+    console.log(this.state.card);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 col-md-offset-3">
+            <div className="m-5">
+              <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
+                <div>
+                  <img src={back} alt="card back" width="253" height="379" onClick={this.handleClick}/>
+                </div>
+                <div>
+                  <img src={this.state.card} alt="card front" width="253" height="379" onClick={this.handleClick}/>
+                </div>  
+              </ReactCardFlip>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
